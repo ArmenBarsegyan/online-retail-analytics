56a602742dc4041560d6b931368520596ba77f1f
ArmenBarsegyan <armenbarsegyan2214@gmail.com>
2026-07-31 15:18:19 +0500

    Delete 03_top_products.sql

--- 03_top_products.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    stock_code,
-    MAX(description) AS description,
-    SUM(quantity) AS units_sold,
-    ROUND(SUM(line_total)::numeric, 0) AS revenue
-FROM orders
-GROUP BY stock_code
-ORDER BY revenue DESC
-LIMIT 10;
