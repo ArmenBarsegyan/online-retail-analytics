b708ee367ce1a93b4e634207402562eff720c784
ArmenBarsegyan <armenbarsegyan2214@gmail.com>
2026-07-31 15:17:12 +0500

    Delete 04_revenue_by_country.sql

--- 04_revenue_by_country.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-    country,
-    COUNT(DISTINCT customer_id) AS customers,
-    ROUND(SUM(line_total)::numeric, 0) AS revenue
-FROM orders
-GROUP BY country
-ORDER BY revenue DESC
-LIMIT 10;
